ca1d337fe76fa44ea5fb541e28a7074adaafe8df
Adam Dishaw <aldishaw@gmail.com>
2015-06-14 00:17:13 -0700

    Use updated systematics including 1b from Ryan and run bulk data card production in parallel

--- run/make_cards.py
+++ run/make_cards.py
@@ -5,11 +5,9 @@ import subprocess
 for sig in ['T1tttt_1500_100','T1tttt_1200_800']:
     for method in range(1,4):
         for lumi in range(1,21):
-            in_name = 'txt/data_card_method_'+str(method)+'_'+sig+'_lumi_'+str(lumi)+'.txt'
-            out_name = 'txt/data_card_method_'+str(method)+'_'+sig+'_lumi_'+str(lumi)+'_systs.txt'
             mlsp = 100
             if sig == 'T1tttt_1200_800':
                 mlsp = 800
-            subprocess.call(['./run/make_card.exe','--lumi',str(lumi),'--method',str(method),'--mlsp',str(mlsp)])
-            out_file = open(out_name,'w')
-            
+            subprocess.Popen(['./run/make_card.exe','--lumi',str(lumi),'--method',str(method),'--mlsp',str(mlsp)])
+            subprocess.Popen(['./run/make_card.exe','--lumi',str(lumi),'--method',str(method),'--mlsp',str(mlsp),'--no_systematics'])
+            subprocess.Popen(['./run/make_card.exe','--lumi',str(lumi),'--method',str(method),'--mlsp',str(mlsp),'--no_mc_kappa'])
