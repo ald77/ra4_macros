70d72cd27d133f39111e2631ad078f39b3c1ee30
Ana Ovcharova <akovcharova@gmail.com>
2015-10-19 20:55:14 -0700

    Updated cutflow table

--- src/table_cutflow.cxx
+++ src/table_cutflow.cxx
@@ -20,9 +20,9 @@ namespace  {
   TString ntuple_date("2015_05_25");
   TString minjets("7"), midjets("9");
   TString mjthresh("400");
-  TString luminosity="10";
+  TString luminosity="3";
   bool do_2l=false;
-  bool do_note=false;
+  bool do_note=true;
 }
 
 using namespace std;
@@ -34,34 +34,42 @@ TString YieldsCut(TString title, TString cuts, vector<TChain*> chain, vector<sfe
 int main(){
 
   // Reading ntuples
-  TString folder="/cms5r0/ald77/archive/"+ntuple_date+"/skim/";
-  TString folder_ns="/cms5r0/ald77/archive/"+ntuple_date+"/";
+  TString folder="/cms2r0/babymaker/babies/2015_10_19/mc/skim_1lht400mc/";
+  TString folder_ns="/cms2r0/babymaker/babies/2015_10_19/mc/";
+  TString folder_data="/cms2r0/babymaker/babies/2015_10_19/data/singlelep/combined/skim_1lht400/";
+  TString folder_data_ns="/cms2r0/babymaker/babies/2015_10_19/data/singlelep/combined/";
+
   vector<TString> s_tt;
-  s_tt.push_back(folder+"*_TTJet*");
+  s_tt.push_back(folder+"*_TTJets*Lept*");
+  s_tt.push_back(folder+"*_TTJets_HT-*");
   vector<TString> s_wjets;
-  s_wjets.push_back(folder+"*_WJets*");
+  s_wjets.push_back(folder+"*_WJetsToLNu_HT*");
   vector<TString> s_single;
-  s_single.push_back(folder+"*_T*channel*");
-  vector<TString> s_ttv;
-  s_ttv.push_back(folder+"*TTW*");
-  s_ttv.push_back(folder+"*TTZ*");
+  s_single.push_back(folder+"*_ST_*");
+  // vector<TString> s_ttv;
+  // s_ttv.push_back(folder+"*TTW*");
+  // s_ttv.push_back(folder+"*TTZ*");
   vector<TString> s_qcd;
-  s_qcd.push_back(folder+"*QCD_Pt*");
-  s_qcd.push_back(folder+"*_TTJet*");
+  s_qcd.push_back(folder+"*_QCD_HT*");
   vector<TString> s_other;
-  s_other.push_back(folder+"*_ZJet*");
-  s_other.push_back(folder+"*DY*");
-  s_other.push_back(folder+"*WH_HToBB*");
+  s_other.push_back(folder+"*_DYJetsToLL_M-50_HT*");
+  // s_other.push_back(folder+"*_ZJet*");
+  s_other.push_back(folder+"*_ttHJetTobb*");
+  s_other.push_back(folder+"*_WWTo2L2Nu*");
+  s_other.push_back(folder+"*_WWToLNuQQ*");
+  s_other.push_back(folder+"*_ggZH_HToBB*");
   vector<TString> s_t1t;
-  s_t1t.push_back(folder+"*T1tttt*1500_*PU20*");
+  s_t1t.push_back(folder+"*-T1tttt_mGluino-1500_mLSP-100*");
   vector<TString> s_t1tc;
-  s_t1tc.push_back(folder+"*T1tttt*1200_*PU20*");
+  s_t1tc.push_back(folder+"*-T1tttt_mGluino-1200_mLSP-800*");
+  // vector<TString> s_trig_sl;
+  // s_trig_sl.push_back(folder_data+"/*Single*");
 
   vector<TChain *> chain, chain_ns;
   vector<sfeats> Samples; 
   Samples.push_back(sfeats(s_other, "Other", 1001));
-  Samples.push_back(sfeats(s_qcd, "QCD", 1005,1,"ntruleps==0")); // It includes hadronic ttbar
-  Samples.push_back(sfeats(s_ttv, "$t\\bar{t}V$", 1002));
+  Samples.push_back(sfeats(s_qcd, "QCD", 1005,1)); 
+  // Samples.push_back(sfeats(s_ttv, "$t\\bar{t}V$", 1002));
   Samples.push_back(sfeats(s_single, "Single $t$", 1005));
   Samples.push_back(sfeats(s_wjets, "W+jets", 1004));
   Samples.push_back(sfeats(s_tt, "$t\\bar{t}$ (1$\\ell$)", 1000,1,"ntruleps==1"));
@@ -69,6 +77,7 @@ int main(){
   int nsig(2);
   Samples.push_back(sfeats(s_t1t, "T1tttt NC", 2));
   Samples.push_back(sfeats(s_t1tc, "T1tttt C", 2,2));
+  // Samples.push_back(sfeats(s_trig_sl, "Data",kBlack,1,"(trig[4]||trig[8])&&nonblind")); Samples.back().isData = true;
 
   TString file_ns;
   for(unsigned sam(0); sam < Samples.size(); sam++){
@@ -77,8 +86,12 @@ int main(){
     for(unsigned insam(0); insam < Samples[sam].file.size(); insam++){
       chain[sam]->Add(Samples[sam].file[insam]);
       file_ns = Samples[sam].file[insam];
-      file_ns.ReplaceAll(folder, folder_ns);
+      if(Samples[sam].isData) file_ns.ReplaceAll(folder_data, folder_data_ns);
+      else file_ns.ReplaceAll(folder, folder_ns);
       chain_ns[sam]->Add(file_ns);
+      // cout<<"Pushing "<<file_ns<<endl;
+      // cout<<chain_ns[sam]->GetEntries()<<" into "<<Samples[sam].label<<endl;
+
     }
   }
 
@@ -115,7 +128,7 @@ int main(){
   cuts += "&&(nmus+nels)==1";
   file << YieldsCut("$1\\ell$", cuts, chain_ns, Samples, nsig);
   cuts += "&ht>500";
-  file << YieldsCut("$H_T>500$ GeV", cuts, chain_ns, Samples, nsig);
+  file << YieldsCut("$H_T>500$ GeV", cuts, chain, Samples, nsig);
   cuts += "&&met>200";
   file << YieldsCut("${\\rm MET}>200$ GeV", cuts, chain, Samples, nsig);
   cuts += "&&njets>="+minjets;
@@ -165,7 +178,7 @@ TString YieldsCut(TString title, TString cuts, vector<TChain*> chain, vector<sfe
       if(yield[sam]>0) bkg += yield[sam];
       bkg_err = sqrt(pow(bkg_err,2)+pow(err,2));
     }
-    //cout<<sam<<": yield "<<Samples[sam].label<<" "<<yield[sam]<<" \t "<<totCut<<endl;
+    // cout<<sam<<": yield "<<Samples[sam].label<<" "<<yield[sam]<<" \t "<<totCut<<endl;
   }
   
   cout<<title<<": B = "<<(RoundNumber(bkg,1)+" +- "+RoundNumber(bkg_err,1));
@@ -174,14 +187,14 @@ TString YieldsCut(TString title, TString cuts, vector<TChain*> chain, vector<sfe
   if(!do_note) out += (" \t &  $"+RoundNumber(bkg,1))+" \\pm "+RoundNumber(bkg_err,1)+"$";
   else out += (" \t &  $"+RoundNumber(bkg,1))+"$";
   for(int sam(nsam-nsig); sam < nsam; sam++) {
-    float fracerr(sqrt(pow(bkg_err/bkg,2)+0.3*0.3+0.24*0.24));
+    // float fracerr(sqrt(pow(bkg_err/bkg,2)+0.3*0.3+0.24*0.24));
     if(!do_note) out += (" \t& $" + RoundNumber(yield[sam],1)+" \\pm "+RoundNumber(error[sam],1))+"$";// + "$ \t& ");
     else out += (" \t& $" + RoundNumber(yield[sam],1))+"$";// + "$ \t& ");
     // if(do_zbi) out += RoundNumber(RooStats::NumberCountingUtils::BinomialExpZ(yield[sam], bkg, fracerr),2);
     // else out += RoundNumber(yield[sam],2,bkg);
 	    
-    cout<<", S = "<<RoundNumber(yield[sam],1)+" +- "+RoundNumber(error[sam],1)<<" with Zbi = ";
-    cout<<RoundNumber(RooStats::NumberCountingUtils::BinomialExpZ(yield[sam], bkg, fracerr),2);
+    // cout<<", S = "<<RoundNumber(yield[sam],1)+" +- "+RoundNumber(error[sam],1)<<" with Zbi = ";
+    // cout<<RoundNumber(RooStats::NumberCountingUtils::BinomialExpZ(yield[sam], bkg, fracerr),2);
   }
   out += " \\\\ \n";
   cout<<endl;
