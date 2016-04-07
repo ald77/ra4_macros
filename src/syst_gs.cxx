7219cee0dfbae3f4df2bcd95a035ddfe2dff3ac9
Rohan Bhandari <rohan.bhandari@cern.ch>
2016-04-06 17:21:21 -0700

    syst_gs.cxx now makes normalized 1D distributions

--- src/syst_gs.cxx
+++ src/syst_gs.cxx
@@ -15,7 +15,7 @@
 namespace{
   const int nmjbins = 3;
   const int nnjetbins = 4;
-  //  bool makePlots=false;
+  bool make1D=true;
   TString plot_type=".pdf";
   TString plot_style="RA4";
   TString outDir="rpv_gs";
@@ -25,6 +25,7 @@ using namespace std;
 
 vector<vector<double> >  getYields(small_tree_rpv &tree, vector<vector<double> >& low_drbb, vector<vector<double> >& high_drbb_err, vector<vector<double> >& low_drbb_err, bool isData=false);
 void printPlots(TGraphErrors *graph, TString title="", TString filename="syst_gs.pdf");
+void print1D(vector<vector<double> > norm);
 double addError(double error, double added_error);
 double divideErrors(double x, double y, double dx, double dy);
 
@@ -34,6 +35,7 @@ int main(){
 
   small_tree_rpv data(folder+"*JetHT_Run2015D-05Oct2015-v1*");
   data.Add(folder+"*JetHT_Run2015D-PromptReco-v4*");
+  data.Add(folder+"*JetHT_Run2015C_25ns-05Oct2015-v1*");
   small_tree_rpv qcd(folder+"*QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*");
   qcd.Add(folder+"*QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*");
   qcd.Add(folder+"*QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8*");
@@ -73,22 +75,18 @@ int main(){
   cout<<"[syst_gs] Finding Normalization..."<<endl;
 
   for(unsigned int i=0; i<data_yields_hdrbb.size(); i++){
-    if(i==0) cout<<"ALL MJ"<<endl;
-    if(i==1) cout<<"LOW MJ"<<endl;
-    if(i==2) cout<<"HIGH MJ"<<endl;
-
     for(unsigned int j=0; j<data_yields_hdrbb[i].size(); j++){
 
       // Subtract yields of other
-            data_yields_hdrbb[i][j] -= other_yields_hdrbb[i][j];
+      data_yields_hdrbb[i][j] -= other_yields_hdrbb[i][j];
       // Uncertainty on subtraction of other
       data_yields_hdrbb_err[i][j] = AddInQuadrature(data_yields_hdrbb_err[i][j], other_yields_hdrbb[i][j]);
-
+      
       // Find normalization in high dr_bb region
       norm_hdrbb[i][j] = data_yields_hdrbb[i][j]/qcd_yields_hdrbb[i][j];
       // Uncertainty on normalization
       norm_hdrbb_err[i][j] = divideErrors(data_yields_hdrbb[i][j], qcd_yields_hdrbb[i][j], data_yields_hdrbb_err[i][j], qcd_yields_hdrbb_err[i][j]);
-      
+
       //Get data/mc in low dr_bb region
       dmc_ldrbb[i][j] = data_yields_ldrbb[i][j]/((norm_hdrbb[i][j]*qcd_yields_ldrbb[i][j]) + other_yields_ldrbb[i][j]); 
       // Uncertainty on data/mc in low dr_bb region
@@ -124,6 +122,11 @@ int main(){
   printPlots(dmc_ldrbb_lowmj, "500 #leq M_{J} #leq 800", "dmc_ldrbb_lowmj.pdf");
   printPlots(dmc_ldrbb_highmj, "M_{J}>800", "dmc_ldrbb_highmj.pdf");
 
+  // print 1D Dists after the normalization in the high dr_bb region
+  if(make1D){
+    cout<<"[syst_gs] Making 1D Distributions... "<<endl;
+    print1D(norm_hdrbb);
+  }
 } 
 
 vector<vector<double> >  getYields(small_tree_rpv &tree, vector<vector<double> >& low_drbb, vector<vector<double> >& high_drbb_err, vector<vector<double> >& low_drbb_err, bool isData){
@@ -350,6 +353,129 @@ void printPlots(TGraphErrors *graph, TString title, TString filename){
   c1.SaveAs("plots/"+filename);
 
   h->Delete();
+}
+
+void print1D(vector<vector<double> > norm){
+
+    string extraWeight("1/eff_trig");
+
+    vector<TString> s_rpv_LO;
+    s_rpv_LO.push_back("/homes/cawest/babymaker/CMSSW_7_4_14/src/babymaker/RPV_M1000_LO.root");
+    vector<TString> s_rpv_LO_M1100;
+    s_rpv_LO_M1100.push_back("/homes/cawest/babymaker/CMSSW_7_4_14/src/babymaker/RPV_M1100_LO.root");
+    vector<TString> s_rpv_NLO;
+    s_rpv_NLO.push_back("/homes/cawest/CMSSW_7_4_14/src/babymaker/RPV_M1000_NLO.root");
+
+    vector<TString> s_tt;
+    //  s_tt.push_back(filestring("TTJets_TuneCUETP8M1_13TeV-madgraphMLM"));
+    s_tt.push_back(filestring("TT_TuneCUETP8M1_13TeV-powheg-pythia8"));
+    vector<TString> s_wjets;
+    s_wjets.push_back(filestring("WJetsToLNu_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    vector<TString> s_singlet;
+    s_singlet.push_back(filestring("ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1"));
+    s_singlet.push_back(filestring("ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1"));
+    s_singlet.push_back(filestring("ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1"));
+    s_singlet.push_back(filestring("ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1"));
+    s_singlet.push_back(filestring("ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1"));
+    vector<TString> s_qcd;
+    s_qcd.push_back(filestring("QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    s_qcd.push_back(filestring("QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    s_qcd.push_back(filestring("QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    vector<TString> s_other;
+    s_other.push_back(filestring("DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    s_other.push_back(filestring("TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8"));
+    s_other.push_back(filestring("TTWJetsToQQ_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8"));
+    s_other.push_back(filestring("TTZToQQ_TuneCUETP8M1_13TeV-amcatnlo-pythia8"));
+    s_other.push_back(filestring("TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8"));
+    s_other.push_back(filestring("ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8"));
+    s_other.push_back(filestring("TTTT_TuneCUETP8M1_13TeV-amcatnlo-pythia8"));
+    vector<TString> s_w_had;
+    s_w_had.push_back(filestring("WJetsToQQ_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"));
+    vector<TString> s_z_had;
+    s_z_had.push_back(filestring("ZJetsToQQ_HT600toInf_13TeV-madgraph"));
+    vector<TString> s_jetht;
+    s_jetht.push_back(filestring("JetHT_Run2015C_25ns-05Oct2015-v1"));
+    s_jetht.push_back(filestring("JetHT_Run2015D-05Oct2015-v1"));
+    s_jetht.push_back(filestring("JetHT_Run2015D-PromptReco-v4"));
+
+    // Reading ntuples
+    vector<sfeats> Samples;
+    Samples.push_back(sfeats(s_jetht, "Data",kBlack,1,"trig[12] && pass && (njets<10 || (nmus+nels)==0)"));
+    Samples.back().isData = true;
+    Samples.back().doStack = false;
+
+    Samples.push_back(sfeats(s_rpv_LO, "#tilde{g}(1.0 TeV)#rightarrow tbs", ra4::c_t1tttt));
+    Samples.back().doStack = false;
+    Samples.back().isSig = true;
+    Samples.push_back(sfeats(s_rpv_LO_M1100, "#tilde{g}(1.1 TeV)#rightarrow tbs", ra4::c_t1tttt, 2));
+    Samples.back().doStack = false;
+    Samples.back().isSig = true;
+
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[0][0])+"/eff_trig"))); // 0 leps, All MJ, 4-5 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[0][1])+"/eff_trig"))); // 0 leps. All MJ, 6-7 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[0][2])+"/eff_trig"))); // 0 leps, All MJ, 8-9 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[0][3])+"/eff_trig"))); // 0 leps, All MJ, 10+ jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[1][0])+"/eff_trig"))); // 0 leps, Low MJ, 4-5 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[1][1])+"/eff_trig"))); // 0 leps. Low MJ, 6-7 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[1][2])+"/eff_trig"))); // 0 leps, Low MJ, 8-9 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[1][3])+"/eff_trig"))); // 0 leps, Low MJ, 10+ jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[2][0])+"/eff_trig"))); // 0 leps, High MJ, 4-5 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[2][1])+"/eff_trig"))); // 0 leps. High MJ, 6-7 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[2][2])+"/eff_trig"))); // 0 leps, High MJ, 8-9 jets
+    Samples.push_back(sfeats(s_qcd, "QCD", kYellow, 1, cutandweight("1",to_string(norm[2][3])+"/eff_trig"))); // 0 leps, High MJ, 10+ jets
+
+    Samples.push_back(sfeats(s_w_had, "W+jets, 0 l", ra4::c_wjets, 1, cutandweight("1",extraWeight)));
+    Samples.push_back(sfeats(s_z_had, "Z+jets, 0 l", kBlack, 1, cutandweight("1",extraWeight)));
+    Samples.push_back(sfeats(s_tt, "t#bar{t}, 1 l", ra4::c_tt_1l, 1, cutandweight("ntruleps==1", extraWeight)));
+    Samples.push_back(sfeats(s_tt, "t#bar{t}, 2 l", ra4::c_tt_2l, 1, cutandweight("ntruleps>=2", extraWeight)));
+    Samples.push_back(sfeats(s_tt, "t#bar{t}, 0 l", kTeal, 1, cutandweight("ntruleps==0", extraWeight)));
+    Samples.push_back(sfeats(s_wjets, "W+jets, 1 l", ra4::c_wjets, 1, cutandweight("1",extraWeight)));
+    Samples.push_back(sfeats(s_singlet, "Single t", ra4::c_singlet, 1, cutandweight("1",extraWeight)));
+    Samples.push_back(sfeats(s_other, "Other", ra4::c_other, 1, cutandweight("1",extraWeight)));
+
+    const int nBins=12;
+    vector<vector<int> >rpv_sam(nBins);
+    for(unsigned sam(0); sam < nBins; sam++){
+      rpv_sam[sam].push_back(0);
+      rpv_sam[sam].push_back(1);
+      rpv_sam[sam].push_back(2);
+
+      rpv_sam[sam].push_back(3+sam); //Only push back for the correct QCD sample corresponding to the bin
+
+      rpv_sam[sam].push_back(15);    //Index should equal 3+nBins, i.e. first sample after QCD
+      rpv_sam[sam].push_back(16);
+      rpv_sam[sam].push_back(17);
+      rpv_sam[sam].push_back(18);
+      rpv_sam[sam].push_back(19);
+      rpv_sam[sam].push_back(20);
+      rpv_sam[sam].push_back(21);
+      rpv_sam[sam].push_back(22);
+    }
+
+    vector<hfeats> vars;
+
+    vector<TString> cuts;
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&njets>=4&&njets<=5&&nbm>=2"); // 0 leps, All MJ, 4-5 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&njets>=6&&njets<=7&&nbm>=2"); // 0 leps, All MJ, 6-7 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&njets>=8&&njets<=9&&nbm>=2"); // 0 leps, All MJ, 8-9 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&njets>=10&&nbm>=2"); // 0 leps, All MJ, 10+ jets
+
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&mj<=800&&njets>=4&&njets<=5&&nbm>=2"); // 0 leps, Low MJ, 4-5 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&mj<=800&&njets>=6&&njets<=7&&nbm>=2"); // 0 leps, Low MJ, 6-7 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&mj<=800&&njets>=8&&njets<=9&&nbm>=2"); // 0 leps, Low MJ, 8-9 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>500&&mj<=800&&njets>=10&&nbm>=2"); // 0 leps, Low MJ, 10+ jets
+
+    cuts.push_back("nleps==0&&ht>1500&&mj>800&&njets>=4&&njets<=5&&nbm>=2"); // 0 leps, High MJ, 4-5 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>800&&njets>=6&&njets<=7&&nbm>=2"); // 0 leps, High MJ, 6-7 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>800&&njets>=8&&njets<=9&&nbm>=2"); // 0 leps, High MJ, 8-9 jets
+    cuts.push_back("nleps==0&&ht>1500&&mj>800&&njets>=10&&nbm>=2"); // 0 leps, High MJ, 10+ jets
+
+
+    for(int iBins=0; iBins<nBins; iBins++){
+      vars.push_back(hfeats("dr_bb",15, 0, 6, rpv_sam[iBins], "#DeltaR_{b#bar{b}}", cuts[iBins]));
+    }
+
+    plot_distributions(Samples, vars, rpv::luminosity, plot_type, plot_style, outDir, true); // last argument determines whether or not a ratio is drawn
 }
 
 double addError(double error, double added_error){
